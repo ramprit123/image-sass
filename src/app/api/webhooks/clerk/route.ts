280de2a4e928c1270195bb0d1803afc8ffe1335c
ramprit123 <ramprit.sahani@outlook.com>
2025-05-26 08:49:39 +0530

    refactor(webhooks): remove unnecessary empty lines in clerk route
    Clean up code by removing redundant empty lines to improve readability and maintain consistency in the codebase.

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -8,7 +8,6 @@ import { Webhook } from "svix";
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
-
   if (!WEBHOOK_SECRET) {
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
@@ -53,7 +52,6 @@ export async function POST(req: Request) {
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
-
   // CREATE
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } =
@@ -69,7 +67,6 @@ export async function POST(req: Request) {
     };
 
     const newUser = await createUser(user);
-
     // Set public metadata
     if (newUser) {
       const client = await clerkClient();
